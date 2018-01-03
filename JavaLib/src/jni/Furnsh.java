4d54793e5ec1117f506c2aa2802d0279cd43c6d6
curtgithub <owner@Owner01.com>
2018-01-03 16:39:13 -0500

    Added JavaLib repository

---
+++ JavaLib/src/jni/Furnsh.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jni;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author owner
+ */
+public class Furnsh {
+
+   public static native void nativeFurnsh(String kernel);
+
+   private static final Logger logger = Logger.getLogger(Furnsh.class.getName());
+
+   static {
+      logger.log(Level.INFO, "java.library.path = "
+              + System.getProperty("java.library.path"));
+      System.loadLibrary("libJinSpic");
+   }
+
+   public static void main(String[] args) {
+      String kernel = "TODO";
+      Furnsh fo = new Furnsh();
+
+   }
+}
