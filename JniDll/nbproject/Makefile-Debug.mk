fbb036a820a9b9add26e62112e612c2133138771
owner <owner@Owner01>
2018-01-04 02:55:30 -0500

    Implemented the call from Java.

--- JniDll/nbproject/Makefile-Debug.mk
+++ JniDll/nbproject/Makefile-Debug.mk
@@ -60,7 +60,7 @@ LDLIBSOPTIONS=
 
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libJniDll.${CND_DLIB_EXT}: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
-	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libJniDll.${CND_DLIB_EXT} ${OBJECTFILES} ${LDLIBSOPTIONS} -shared
+	x86_64-w64-mingw32-g++.exe -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libJniDll.${CND_DLIB_EXT} ${OBJECTFILES} ${LDLIBSOPTIONS} -shared
 
 ${OBJECTDIR}/_ext/9a46fe2b/jorbit_spice_jni_Furnsh.o: /cygdrive/D/JOrbit/Spice/JniDll/src/jorbit_spice_jni_Furnsh.c 
 	${MKDIR} -p ${OBJECTDIR}/_ext/9a46fe2b
--- JniDll/src/jorbit_spice_jni_Furnsh.c
+++ JniDll/src/jorbit_spice_jni_Furnsh.c
@@ -1,6 +1,22 @@
 #include "jorbit_spice_jni_Furnsh.h"
 
 JNIEXPORT void JNICALL Java_jorbit_spice_jni_Furnsh_cFurnsh
-  (JNIEnv *je, jclass jc, jstring js) {
-   
+(JNIEnv *je, jclass jc, jstring kernel) {
+
+   //
+   // Get a local copy of the kernel string
+   //
+   jboolean iscopy;
+   const char* ns = (*je)->GetStringUTFChars(je, kernel, &iscopy);
+    
+   //
+   // Send kernel string to FURNSH_C
+   //
+   printf("INFO: Java_jorbit_spice_jni_Furnsh_cFurnsh: kernel = %s\n", ns);
+
+   //
+   // Release the kernel string.
+   //
+   (*je)->ReleaseStringUTFChars(je, kernel, ns);
+
 }
