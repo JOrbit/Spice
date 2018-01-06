dba192dc66cd3062f05487a6bd09f81a7a317e8f
owner <owner@Owner01>
2018-01-06 18:26:14 -0500

    Calls GetEt.

--- ClientMain/src/clientmain/ClientMain.java
+++ ClientMain/src/clientmain/ClientMain.java
@@ -5,14 +5,7 @@
  */
 package clientmain;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import spice.GetSocket;
+import spice.GetEt;
 
 /**
  *
@@ -24,45 +17,8 @@ public class ClientMain {
     * @param args the command line arguments
     */
    public static void main(String[] args) {
-      int nbytes = 1024;
-      byte[] ibytes = new byte[nbytes];
-      String msg;
-      Logger logger = Logger.getLogger("ClientMain");
 
-      try {
+      double et = GetEt.getEt("2005 SEP 02 04:50:45");
 
-         msg = "Connected to server: " + GetSocket.getSocket().getRemoteSocketAddress();
-         logger.log(Level.INFO, msg);
-         msg = "Server = " + GetSocket.getSocket().toString();
-         logger.log(Level.INFO, msg);
-
-         InputStream inFromServer = GetSocket.getSocket().getInputStream();
-         DataInputStream in = new DataInputStream(inFromServer);
-         OutputStream outToServer = GetSocket.getSocket().getOutputStream();
-         DataOutputStream out = new DataOutputStream(outToServer);
-
-         for (int i = 0; i < 10; i++) {
-
-            String omsg = "Requst #" + (i + 1);
-            byte[] obytes = omsg.getBytes();
-            out.write(obytes, 0, obytes.length);
-            msg = "Sent to server -> " + omsg;
-            logger.log(Level.INFO, msg);
-
-            nbytes = in.read(ibytes, 0, ibytes.length);
-            msg = "Read nbytes = " + nbytes;
-            logger.log(Level.INFO, msg);
-            String imsg = new String(ibytes, 0, nbytes);
-            imsg = imsg + "\n";
-            msg = "Received message -> " + imsg;
-            logger.log(Level.INFO, msg);
-
-         }
-
-         GetSocket.getSocket().close();
-      } catch (IOException ex) {
-         msg = "Server is not running.";
-         logger.log(Level.SEVERE, msg);
-      }
    }
 }
