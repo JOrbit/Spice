0cf0527f5df355ef902797cf3436b09e01fcdf70
owner <owner@Owner01>
2018-01-16 20:36:36 -0500

    Working on parsing spice commands.

--- ClientMain/src/clientmain/ClientMain.java
+++ ClientMain/src/clientmain/ClientMain.java
@@ -6,6 +6,7 @@
 package clientmain;
 
 import java.net.Socket;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import spice.GetSocket;
 import spice.SpiceCommands;
@@ -22,11 +23,23 @@ public class ClientMain {
     * @param args the command line arguments
     */
    public static void main(String[] args) {
+
       Socket socket = GetSocket.getSocket();
-      
+      byte[] receive = new byte[GetSocket.BSIZE];
+
+      LOG.log(Level.INFO, "Sending list of spice commands");
       byte[] send = SpiceCommands.toBytes();
-      byte[] received = GetSocket.sendReceive(socket, send);
-      
+      byte[] received = GetSocket.sendReceive(socket, send, receive);
+      String msg = new String(received, 0, received.length);
+      LOG.log(Level.INFO, "Received = " + msg);
+
+      String spiceCommand = "BODVRD";
+      LOG.log(Level.INFO, "Sending spice command = " + spiceCommand);
+      send = spiceCommand.getBytes();
+      received = GetSocket.sendReceive(socket, send, receive);
+      msg = new String(received, 0, received.length);
+      LOG.log(Level.INFO, "Received = " + msg);
+
       GetSocket.closeSocket();
    }
 }
