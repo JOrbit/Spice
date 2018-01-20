cb55b8303cc3024f2349c64d26e035a4b14a8ba2
owner <owner@Owner01>
2018-01-19 23:42:23 -0500

    Bodvrd, Furnsh, Spkezr, Str2et, working!

--- ClientMain/src/clientmain/ClientMain.java
+++ ClientMain/src/clientmain/ClientMain.java
@@ -7,8 +7,10 @@ package clientmain;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import jspice.Bodvrd;
 import jspice.Furnsh;
 import jspice.GetSocket;
+import jspice.Spkezr;
 import jspice.Str2et;
 
 /**
@@ -26,26 +28,44 @@ public class ClientMain {
 
       String lskFile = "D:/naif/Kernels/MEX/lsk/naif0008.tls";
       Furnsh furnshLsk = new Furnsh(lskFile);
-      LOG.log(Level.INFO, "Sending spice command = " + furnshLsk);
-      String msg = furnshLsk.process();
-      LOG.log(Level.INFO, "Received = " + msg);
+      furnshLsk.process();
 
       int year = 1017;
       String utc = null;
-      for (int i = 0; i < 1000; i++) {
+      for (int i = 0; i < 1; i++) {
          utc = new String(Integer.toString(year));
          utc = utc + " SEP 02 04:50:45";
-         LOG.log(Level.INFO, "utc = " + utc);
          year++;
          Str2et str2etUtc = new Str2et(utc);
-         LOG.log(Level.INFO, "Sending spice command = " + str2etUtc);
-         msg = str2etUtc.process();
-         LOG.log(Level.INFO, "Received = " + msg);
-         LOG.log(Level.INFO, "str2etUtc.getEt() = " + str2etUtc.getEt());
-
+         str2etUtc.process();
+         LOG.log(Level.INFO, "bodvrd.getGm() = " + str2etUtc.getEt());
          LOG.log(Level.INFO, "runs = " + (i + 1));
-
       }
+
+      String pckFile = "D:/naif/Kernels/MEX/pck/DE403-MASSES.TPC";
+      Furnsh furnshPck = new Furnsh(pckFile);
+      furnshPck.process();
+
+      Bodvrd bodvrd = new Bodvrd("MARS");
+      bodvrd.process();
+      LOG.log(Level.INFO, "bodvrd.getGm() = " + bodvrd.getGm());
+
+      String spkFile = "D:/naif/Kernels/MEX/spk/ORMM__050901000000_00165.BSP";
+      Furnsh furnshSpk = new Furnsh(spkFile);
+      furnshSpk.process();
+
+      String target = "EARTH";
+      double et = 0.0;
+      String refFrame = "J2000";
+      String abcorr = "NONE";
+      String observer = "SUN";
+      Spkezr spkezr = new Spkezr(target, et, refFrame, abcorr, observer);
+      spkezr.process();
+      for (int i = 0; i < spkezr.getStateVectors().length; i++) {
+         LOG.log(Level.INFO, "stageVectors[" + i + "]"
+                 + spkezr.getStateVectors()[i]);
+      }
+      LOG.log(Level.INFO, "lt = " + spkezr.getLt());
 
       GetSocket.closeSocket();
    }
