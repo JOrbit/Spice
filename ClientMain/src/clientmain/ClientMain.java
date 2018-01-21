4440a604ce643eb5409f7ea7feb07c895d436721
owner <owner@Owner01>
2018-01-20 21:20:30 -0500

    Bodvrd, Furnsh, Spkezr, Str2et, working for Earth Sun.  Created an EarthSun.tm file so only one call is needed.

--- ClientMain/src/clientmain/ClientMain.java
+++ ClientMain/src/clientmain/ClientMain.java
@@ -26,9 +26,9 @@ public class ClientMain {
     */
    public static void main(String[] args) {
 
-      String lskFile = "D:/naif/Kernels/MEX/lsk/naif0008.tls";
-      Furnsh furnshLsk = new Furnsh(lskFile);
-      furnshLsk.process();
+      String earthSunTm = "D:/naif/Kernels/EarthSun.tm";
+      Furnsh furnshEarthSunTm = new Furnsh(earthSunTm);
+      furnshEarthSunTm.process();
 
       int year = 1017;
       String utc = null;
@@ -38,22 +38,14 @@ public class ClientMain {
          year++;
          Str2et str2etUtc = new Str2et(utc);
          str2etUtc.process();
-         LOG.log(Level.INFO, "bodvrd.getGm() = " + str2etUtc.getEt());
+         LOG.log(Level.INFO, "str2etUtc.getEt() = " + str2etUtc.getEt());
          LOG.log(Level.INFO, "runs = " + (i + 1));
       }
-
-      String pckFile = "D:/naif/Kernels/MEX/pck/DE403-MASSES.TPC";
-      Furnsh furnshPck = new Furnsh(pckFile);
-      furnshPck.process();
-
-      Bodvrd bodvrd = new Bodvrd("MARS");
+      
+      Bodvrd bodvrd = new Bodvrd("EARTH");
       bodvrd.process();
       LOG.log(Level.INFO, "bodvrd.getGm() = " + bodvrd.getGm());
-
-      String spkFile = "D:/naif/Kernels/MEX/spk/ORMM__050901000000_00165.BSP";
-      Furnsh furnshSpk = new Furnsh(spkFile);
-      furnshSpk.process();
-
+      
       String target = "EARTH";
       double et = 0.0;
       String refFrame = "J2000";
@@ -62,7 +54,7 @@ public class ClientMain {
       Spkezr spkezr = new Spkezr(target, et, refFrame, abcorr, observer);
       spkezr.process();
       for (int i = 0; i < spkezr.getStateVectors().length; i++) {
-         LOG.log(Level.INFO, "stageVectors[" + i + "]"
+         LOG.log(Level.INFO, "stageVectors[" + i + "] = "
                  + spkezr.getStateVectors()[i]);
       }
       LOG.log(Level.INFO, "lt = " + spkezr.getLt());
