1af8705b62d6617026ce3ef4cd5ce71ae5433089
owner <owner@Owner01>
2018-10-28 01:58:21 -0400

    Adding NetBeans project.

---
+++ SpiceOrbitalElements/main.c
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: owner
+ *
+ * Created on October 28, 2018, 1:39 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+   
+   printf("SpiceOrbitalElements\n");
+
+   return (EXIT_SUCCESS);
+}
+
